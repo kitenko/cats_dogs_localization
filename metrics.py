e27292c3c54f55a75bd2f170126dadc2f3e6477f
андрей китенко <genialniy.kitenko@gmail.com>
2021-05-24 22:13:25 +0300

    added IoURectangle, Accuracy

--- metrics.py
+++ metrics.py
@@ -1,16 +1,13 @@
 from typing import Tuple
 from abc import abstractmethod
 
-import numpy as np
-
 import tensorflow as tf
 
-from config import INPUT_SHAPE
+
 class Metric:
     def __init__(self, num_classes: int, is_binary_cross_entropy: bool = False) -> None:
         """
-        # заменить текст
-        Metrics are counted False Positive, False Negative, True Positive.
+        This class is the parent class for the(Recall, Precision, F1Score) classes.
 
         :param num_classes: number of classes in the dataset.
         :param is_binary_cross_entropy: If there are no more than two classes, the value is set to True.
@@ -63,9 +60,9 @@ class Recall(Metric):
     def __call__(self, y_true: tf.Tensor, y_pred: tf.Tensor) -> tf.Tensor:
         """
 
-       :param y_true: This is the true mark of validation data.6
-       :param y_pred: This is the predict mark of validation data.
-       :return: recall recall metric.
+       :param y_true: This is the true mark of data.
+       :param y_pred: This is the predict mark of data.
+       :return: recall metric.
        """
         fp, fn, tp = self.confusion_matrix(y_true, y_pred)
         return tp / (tp + fn + self.epsilon)
@@ -85,8 +82,8 @@ class Precision(Metric):
     def __call__(self, y_true: tf.Tensor, y_pred: tf.Tensor) -> tf.Tensor:
         """
 
-        :param y_true: This is the true mark of validation data.
-        :param y_pred: This is the predict mark of validation data.6
+        :param y_true: This is the true mark of data.
+        :param y_pred: This is the predict mark of data.
         :return: precision metric.
         """
         fp, fn, tp = self.confusion_matrix(y_true, y_pred)
@@ -110,8 +107,8 @@ class F1Score(Metric):
     def __call__(self, y_true: tf.Tensor, y_pred: tf.Tensor) -> tf.Tensor:
         """
 
-        :param y_true: This is the true mark of validation data.
-        :param y_pred: This is the predict mark of validation data.6
+        :param y_true: This is the true mark of data.
+        :param y_pred: This is the predict mark of data.
         :return: f1score metric.
         """
         fp, fn, tp = self.confusion_matrix(y_true, y_pred)
@@ -120,35 +117,65 @@ class F1Score(Metric):
         return (self.beta ** 2 + 1) * precision * recall / (self.beta ** 2 * precision + recall + self.epsilon)
 
 
-class IoU(Metric):
-    def __init__(self, num_classes: int, is_binary_cross_entropy: bool = False):
-        super().__init__(num_classes, is_binary_cross_entropy)
-        self.__name__ = 'IoU'
+class IoURectangle(tf.keras.metrics.Metric):
+    def __init__(self, name: str = 'iou', **kwargs) -> None:
+        """
 
-    def __call__(self, y_true: tf.Tensor, y_pred: tf.Tensor) -> tf.Tensor:
+        :param name: this name metric.
+        """
+        super(IoURectangle, self).__init__(name=name, **kwargs)
+        self.ious = self.add_weight(name='iou_rectangle', initializer='zeros')
+        self.batches = self.add_weight(name='batch', initializer='zeros')
 
-        print(y_true, y_pred)
-        # determine the (x, y)-coordinates of the intersection rectangle
-        xTrue = tf.math.maximum(y_true[:, 1], y_pred[:, 1])
-        yTrue = tf.math.maximum(y_true[:, 2], y_pred[:, 2])
-        xPred = tf.math.minimum(y_true[:, 3], y_pred[:, 3])
-        yPred = tf.math.minimum(y_true[:, 4], y_pred[:, 4])
+    def update_state(self, y_true: tf.Tensor, y_pred: tf.Tensor, sample_weight=None) -> None:
+        """
+        This method computes the area of intersection rectangle, compute the area of both the prediction and
+        ground-truth rectangles and computes the intersection over union by taking the intersection area and
+        dividing it by the sum of prediction + ground-truth areas - the intersection area.
 
-        one = tf.ones(1)
+        :param y_true: This is the true mark of data.
+        :param y_pred: This is the predict mark of data.
+        :param sample_weight:
+        """
+        x_max = tf.math.maximum(y_true[:, 1], y_pred[:, 1])
+        y_max = tf.math.maximum(y_true[:, 2], y_pred[:, 2])
+        x_min = tf.math.minimum(y_true[:, 3], y_pred[:, 3])
+        y_min = tf.math.minimum(y_true[:, 4], y_pred[:, 4])
 
-        # compute the area of intersection rectangle
-        interArea = tf.math.maximum(tf.zeros(1), xPred - xTrue + tf.ones(1)) * tf.math.maximum(tf.zeros(1), yPred - yTrue + tf.ones(1))
+        inter_area = (tf.math.maximum(tf.zeros(1), x_min - x_max + tf.ones(1)) *
+                      tf.math.maximum(tf.zeros(1), y_min - y_max + tf.ones(1)))
 
-        # compute the area of both the prediction and ground-truth
-        # rectangles
-        boxAArea = (y_true[:, 3] - y_true[:, 1] + tf.ones(1)) * (y_true[:, 4] - y_true[:, 2] + tf.ones(1))
-        boxBArea = (y_pred[:, 3] - y_pred[:, 1] + tf.ones(1)) * (y_pred[:, 4] - y_pred[:, 2] + tf.ones(1))
+        first_area = (y_true[:, 3] - y_true[:, 1] + tf.ones(1)) * (y_true[:, 4] - y_true[:, 2] + tf.ones(1))
+        second_area = (y_pred[:, 3] - y_pred[:, 1] + tf.ones(1)) * (y_pred[:, 4] - y_pred[:, 2] + tf.ones(1))
 
-        # compute the intersection over union by taking the intersection
-        # area and dividing it by the sum of prediction + ground-truth
-        # areas - the interesection area
-        # iou = tf.cast(interArea, tf.float32) / tf.cast((tf.cast(boxAArea, tf.float32) + tf.cast(boxBArea, tf.float32)
-        #                                                 - tf.cast(interArea, tf.float32)), tf.float32)
-        iou = interArea / (boxAArea + boxBArea - interArea)
+        iou = inter_area / (first_area + second_area - inter_area)
+        self.ious.assign_add(tf.reduce_sum(iou))
+        self.batches.assign_add(tf.cast(tf.shape(iou)[0], tf.float32))
 
-        return iou
+    def result(self):
+        return self.ious / self.batches
+
+    def reset_state(self):
+        self.ious.assign(0)
+        self.batches.assign(0)
+
+
+class Accuracy(tf.keras.metrics.BinaryAccuracy):
+    def __init__(self, name='accuracy_custom', **kwargs):
+        """
+
+        :param name: this name metric.
+        """
+        super(Accuracy, self).__init__(name=name, **kwargs)
+
+    def update_state(self, y_true: tf.Tensor, y_pred: tf.Tensor, sample_weight=None):
+        """
+        This method computes accuracy only for class.
+
+        :param y_true: This is the true mark of data.
+        :param y_pred: This is the predict mark of data.
+        :param sample_weight:
+        """
+        y_true = tf.expand_dims(y_true[:, 0], axis=1)
+        y_pred = tf.expand_dims(y_pred[:, 0], axis=1)
+        return super(Accuracy, self).update_state(y_true, y_pred, sample_weight)
