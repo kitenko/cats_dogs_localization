b3aa43cca06f3717b89638f12bc0bc4aaa885977
андрей китенко <genialniy.kitenko@gmail.com>
2021-06-21 19:17:39 +0300

    added show_batch() changed gpu use mod

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import os
 import time
 import argparse
 from tqdm import tqdm
@@ -6,27 +7,10 @@ from typing import Tuple, List
 import cv2
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
-from src import build_model
-from src import DataGenerator
-from config import INPUT_SHAPE
-from src import Accuracy, IoURectangle
-
-
-def parse_args() -> argparse.Namespace:
-    """
-    Parsing command line arguments with argparse.
-    """
-    parser = argparse.ArgumentParser('script for model testing.')
-    parser.add_argument('--weights', type=str, default=None, help='Path for loading model weights.')
-    parser.add_argument('--webcam', action='store_true', help='If the value is True, then the webcam will be used for '
-                                                              'the test.')
-    parser.add_argument('--metrics', action='store_true', help='If the value is True, then the average metrics on the '
-                                                               'validation dataset will be calculated.')
-    parser.add_argument('--time', action='store_true', help='If the value is True, then the inference time and the '
-                                                            'average fps on the validation dataset will be calculated.')
-    parser.add_argument('--gpu', action='store_true', help='If True, then the gpu is used for the test.')
-    return parser.parse_args()
+from config import INPUT_SHAPE, AUGMENTATION_DATA
+from src import build_model, DataGenerator, Accuracy, IoURectangle
 
 
 def preparing_frame(image: np.ndarray, model: tf.keras.Model, cam_width: float, cam_height: float) -> Tuple[List[int],
@@ -34,8 +18,8 @@ def preparing_frame(image: np.ndarray, model: tf.keras.Model, cam_width: float, 
     """
     This function prepares the image and makes a prediction.
 
-    :param cam_height:
-    :param cam_width:
+    :param cam_height: this is height of the input image.
+    :param cam_width: this is width of the input image.
     :param image: this is input image or frame.
     :param model: model with loaded weights.
     :return: bounding_box and class for image.
@@ -58,8 +42,8 @@ def visualization() -> None:
     This function captures webcam video and resizes the image.
     """
     cat_dog = {0: 'cat', 1: 'dog'}
-    BOX_COLOR = (255, 0, 0)  # Red
-    TEXT_COLOR = (255, 255, 255)  # White
+    BOX_COLOR = (255, 0, 0)
+    TEXT_COLOR = (255, 255, 255)
 
     model = build_model()
     model.load_weights(args.weights)
@@ -99,13 +83,15 @@ def visualization() -> None:
     cv2.destroyAllWindows()
 
 
-def test_metrics_and_time(mode: str) -> None:
+def test_metrics_and_time(mode: str, data: str) -> None:
     """
     This function calculates the average value of loss and metrics as well as inference time and average fps.
 
     :param mode: depending on the mode ('metrics', 'time'), the function counts (loss, metrics) or time and average fps.
+    :param data: this is path for data.
     """
-    data_gen = DataGenerator(batch_size=1, is_train=False)
+    data_gen = DataGenerator(batch_size=1, is_train=False, val_data=os.path.join(data, 'valid'),
+                             train_data=os.path.join(data, 'train'))
     model = build_model()
     model.load_weights(args.weights)
     model.compile(loss=tf.keras.losses.binary_crossentropy, metrics=[Accuracy(), IoURectangle()])
@@ -120,7 +106,7 @@ def test_metrics_and_time(mode: str) -> None:
         for i in tqdm(range(len(data_gen))):
             images, _ = data_gen[i]
             start_time = time.time()
-            model.predict(images)
+            model.predict_on_batch(images)
             finish_time = time.time()
             all_times.append(finish_time - start_time)
         all_times = all_times[5:]
@@ -130,16 +116,92 @@ def test_metrics_and_time(mode: str) -> None:
         print(message)
 
 
+def show_batch(data: str):
+    """
+    This function displays the batches generated in data_generator.
+    """
+    box_color = (255, 0, 0)
+    text_color = (255, 255, 255)
+    data_gen = DataGenerator(val_data=os.path.join(data, 'valid'), train_data=os.path.join(data, 'train'),
+                             is_train=True)
+    for i in range(len(data_gen)):
+
+        images, labels = data_gen[i]
+        cat_dog = {0: 'cat', 1: 'dog'}
+        rows_columns_subplot = data_gen.batch_size
+
+        while np.math.sqrt(rows_columns_subplot) - int(np.math.sqrt(rows_columns_subplot)) != 0.0:
+            rows_columns_subplot += 1
+        rows_columns_subplot = int(np.math.sqrt(rows_columns_subplot))
+        plt.figure(figsize=[20, 20])
+        for i, j in enumerate(images):
+            x_min, y_min, x_max, y_max = (int(INPUT_SHAPE[0] * labels[i, 1]),
+                                          int(INPUT_SHAPE[0] * labels[i, 2]),
+                                          int(INPUT_SHAPE[0] * labels[i, 3]),
+                                          int(INPUT_SHAPE[0] * labels[i, 4]))
+            cv2.rectangle(j, (x_min, y_min), (x_max, y_max), color=box_color, thickness=2)
+            ((text_width, text_height), _) = cv2.getTextSize(cat_dog[int(labels[i][0])],
+                                                             cv2.FONT_HERSHEY_SIMPLEX, 0.35, 1)
+            cv2.rectangle(j, (x_min, y_min - int(1.3 * text_height)), (x_min + text_width, y_min), box_color, -1)
+            cv2.putText(
+                j,
+                text=cat_dog[labels[i][0]],
+                org=(x_min, y_min - int(0.3 * text_height)),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                fontScale=0.35,
+                color=text_color,
+                lineType=cv2.LINE_AA,
+            )
+            plt.subplot(rows_columns_subplot, rows_columns_subplot, i + 1)
+            plt.imshow(j)
+            if AUGMENTATION_DATA:
+                plt.title('Augmented, class = "{}"'.format(cat_dog[labels[i][0]]))
+            else:
+                plt.title('Original, class = "{}"'.format(cat_dog[labels[i][0]]))
+        if plt.waitforbuttonpress(0):
+            plt.close('all')
+            raise SystemExit
+        plt.close()
+
+
+def parse_args() -> argparse.Namespace:
+    """
+    Parsing command line arguments with argparse.
+    """
+    parser = argparse.ArgumentParser('script for model testing.')
+    parser.add_argument('--weights', type=str, default=None, help='Path for loading model weights.')
+    parser.add_argument('--webcam', action='store_true', help='If the value is True, then the webcam will be used for '
+                                                              'the test.')
+    parser.add_argument('--metrics', action='store_true', help='If the value is True, then the average metrics on the '
+                                                               'validation dataset will be calculated.')
+    parser.add_argument('--data', type=str, default='cats_dogs_dataset', help='Path for testing data.')
+    parser.add_argument('--time', action='store_true', help='If the value is True, then the inference time and the '
+                                                            'average fps on the validation dataset will be calculated.')
+    parser.add_argument('--gpu', type=str, default='_', help='If you want to use the GPU, you must specify the number '
+                                                             'of the video card that you want to use.')
+    parser.add_argument('--batch', action='store_true', help='If True, the screen will display batches '
+                                                             'from data_generator.')
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
     args = parse_args()
 
-    if args.gpu is True:
-        devices = tf.config.experimental.list_physical_devices('GPU')
-        tf.config.experimental.set_memory_growth(devices[0], True)
+    if args.gpu != '_':
+        gpus_list = [int(args.gpu)]
+    else:
+        gpus_list = []
+    devices = tf.config.get_visible_devices('GPU')
+    devices = [devices[i] for i in gpus_list]
+    tf.config.set_visible_devices(devices, 'GPU')
+    for gpu in devices:
+        tf.config.experimental.set_memory_growth(gpu, True)
 
-    if args.webcam is True:
+    if args.webcam:
         visualization()
-    if args.metrics is True:
-        test_metrics_and_time('metrics')
-    if args.time is True:
-        test_metrics_and_time('time')
+    if args.metrics:
+        test_metrics_and_time('metrics', args.data)
+    if args.time:
+        test_metrics_and_time('time', args.data)
+    if args.batch:
+        show_batch(args.data)
